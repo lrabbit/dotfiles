380dfea4649c52a4fa3529009309dc1b20711b32
lrabbit <hakim.mazouz@gmail.com>
2015-03-03 21:08:07 +0100

    initial import

---
+++ .bashrc
@@ -0,0 +1,7 @@
+# .bashrc
+
+# User specific aliases and functions
+alias nano='nano -cw'
+
+# send Ctrl+Alt+Fx to the apps
+setxkbmap -option "srvrkeys:none"
---
+++ profile.d/java.sh
@@ -0,0 +1,4 @@
+export JAVA_FONTS=/usr/share/fonts 
+export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=lcd'
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0
+export PATH=$PATH:$JAVA_HOME
